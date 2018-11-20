0fcdfd6c3cda7e2d96f2dadb2afef8b51e30c612
ann <gazmaket@gmail.com>
2018-11-20 21:21:57 +0200

    with filling, change init

--- init.sql
+++ init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Med(
-  name char(50),
+  name char(300),
   id int primary key
 );
 
@@ -11,7 +11,7 @@ CREATE TABLE Equipment(
 CREATE TABLE Animal(
   name char(50),
   id int primary key,
-  sex char
+  sex char(10)
 );
 
 CREATE TABLE Client(
@@ -23,14 +23,15 @@ CREATE TABLE Client(
 
 CREATE TABLE Entry(
   client_name char(50) REFERENCES Client(name),
+--   изменила, связь построена не правильно она маст би через принадлежание животного клиенту
   animal_id int REFERENCES Animal(id),
   data timestamp,
   reason text,
   id int primary key
 );
-
+-- изменила float на тип money
 CREATE TABLE Bill(
-  sum float,
+  sum money,
   id int primary key,
   entry_id int REFERENCES Entry(id)
 );
@@ -43,12 +44,15 @@ CREATE TABLE Doctor(
   work_end time
 );
 
+-- primary key в таком виде как был тут не нужен, т.к он мешает делать отношение 1 к н
 CREATE TABLE What_med_have(
-  doctor_id int primary key REFERENCES Doctor(id),
-  med_id int REFERENCES Med(id)
+  doctor_id int REFERENCES Doctor(id),
+  med_id int REFERENCES Med(id),
+  primary key(doctor_id,med_id)
 );
 
 CREATE TABLE What_eq_have(
-  doctor_id int primary key REFERENCES Doctor(id),
-  eq_id int REFERENCES Equipment(id)
+  doctor_id int REFERENCES Doctor(id),
+  eq_id int REFERENCES Equipment(id),
+  primary key (doctor_id, eq_id)
 );
